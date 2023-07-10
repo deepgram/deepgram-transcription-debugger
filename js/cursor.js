7d26a0125003a855879cd88c7dd6b71fbdbd7f65
DamienDeepgram <138720050+DamienDeepgram@users.noreply.github.com>
2023-07-10 01:39:49 -0700

    Create cursor.js

---
+++ js/cursor.js
@@ -0,0 +1,10 @@
+const CURSOR = WaveSurfer.cursor.create({
+    showTime: true,
+    opacity: 1,
+    customShowTimeStyle: {
+        'background-color': '#000',
+        color: '#fff',
+        padding: '2px',
+        'font-size': '10px'
+    }
+})
